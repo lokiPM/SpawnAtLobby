2ef3de4893bbdd2be05074b0a3070168df739c04
lokiPM <142106680+lokiPM@users.noreply.github.com>
2025-02-16 11:34:18 +0100

    Added new Function

---
+++ src/Main.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace lokiPM\lubroLobby;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\math\Vector3;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(): void{
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onJoin(PlayerJoinEvent $event): void{
+        $player = $event->getPlayer();
+        
+        $worldName = $this->getConfig()->get("world", "lobby");
+        $positionString = $this->getConfig()->get("position", "0 0 0");
+        
+        if ($worldName === "world" && $positionString === "0 0 0") {
+            return; // Keine Aktion, wenn die Config "world" und "0 0 0" ist
+        }
+        
+        $positionArray = explode(" ", $positionString);
+        
+        if (count($positionArray) === 3) {
+            $x = (float) $positionArray[0];
+            $y = (float) $positionArray[1];
+            $z = (float) $positionArray[2];
+
+            $world = $this->getServer()->getWorldManager()->getWorldByName($worldName);
+            if ($world !== null) {
+                $player->teleport(new Vector3($x, $y, $z));
+            }
+        }
+    }
+}
